4fd5e890fecc7973f598bf0fd02f9516f572eed8
heejin <heejin0857@naver.com>
2024-05-24 17:51:24 +0900

    disabled tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import CustomTooltip from "./components/Example/CustomTooltip/CustomTooltip";
+import Disable from "./components/Example/Disable/Disable";
 import Normal from "./components/Example/Normal/Normal";
 import Options from "./components/Example/Options/Options";
+import Scroll from "./components/Example/Scroll/Scroll";
 import VariousContent from "./components/Example/VariousContent/VariousContent";
 
 function App() {
     return (
         <div style={{ backgroundColor: "#f4f7ff", height: "100vh" }}>
-            <Normal />
+            <div style={{ display: "flex", gap: 30 }}>
+                <Normal />
+                <Scroll />
+            </div>
             <Options />
             <VariousContent />
             <CustomTooltip />
+            <Disable />
         </div>
     );
 }
---
+++ src/components/Example/Disable/Disable.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import * as S from "./Disable.styels";
+import Tooltip from "../../Tooltip/Tooltip";
+
+export default function Disable() {
+    const [disable, setDisable] = useState(false);
+    return (
+        <S.Container>
+            <S.Button onClick={() => setDisable(!disable)} disable={disable}>
+                {disable ? "Enable" : "Disable"}
+            </S.Button>
+            <Tooltip
+                disabled={disable}
+                direction="top"
+                content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+asperiores atque"
+                style={{ width: "200px" }}
+            >
+                <S.Content>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci asperiores atque
+                </S.Content>
+            </Tooltip>
+        </S.Container>
+    );
+}
