48f9da5b764d0d52ea50e9e964a8274c63dd7323
heejin <heejin0857@naver.com>
2024-05-25 21:20:27 +0900

    overflow scroll also tooltip show

---
+++ src/components/Example/Scroll/Scroll.styles.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const TopContainer = styled.div`
+    margin-bottom: 80px;
+    white-space: nowrap;
+`;
+export const RightContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+export const LeftContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+export const BottomContainer = styled.div``;
+
+export const Content = styled.div`
+    background-color: #50a88c;
+    width: 70px;
+    text-align: center;
+    color: white;
+    font-weight: bolder;
+    border-radius: 4px;
+    border: none;
+    font-size: 15px;
+    padding: 2px 0;
+    margin: 2px;
+    cursor: pointer;
+    transition: 800ms ease all;
+`;
---
+++ src/components/Example/Scroll/Scroll.tsx
@@ -0,0 +1,56 @@
+import Tooltip from "../../Tooltip/Tooltip";
+import * as S from "./Scroll.styles";
+import Prompt from "../Prompt";
+
+export default function Scroll() {
+    return (
+        <S.Container>
+            <S.LeftContainer>
+                <Tooltip direction="leftTop" content={<Prompt />}>
+                    <S.Content className="content">LT</S.Content>
+                </Tooltip>
+                <Tooltip direction="left" content={<Prompt />}>
+                    <S.Content className="content">Left</S.Content>
+                </Tooltip>
+                <Tooltip direction="leftBottom" content={<Prompt />}>
+                    <S.Content className="content">LB</S.Content>
+                </Tooltip>
+            </S.LeftContainer>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                <S.TopContainer>
+                    <Tooltip direction="topLeft" content={<Prompt />}>
+                        <S.Content className="content">TL</S.Content>
+                    </Tooltip>
+                    <Tooltip direction="top" content={<Prompt />}>
+                        <S.Content className="content">Top</S.Content>
+                    </Tooltip>
+                    <Tooltip direction="topRight" content={<Prompt />}>
+                        <S.Content className="content">TR</S.Content>
+                    </Tooltip>
+                </S.TopContainer>
+                <S.BottomContainer>
+                    <Tooltip direction="bottomLeft" content={<Prompt />}>
+                        <S.Content className="content">BL</S.Content>
+                    </Tooltip>
+                    <Tooltip direction="bottom" content={<Prompt />}>
+                        <S.Content className="content">Bottom</S.Content>
+                    </Tooltip>
+                    <Tooltip direction="bottomRight" content={<Prompt />}>
+                        <S.Content className="content">BR</S.Content>
+                    </Tooltip>
+                </S.BottomContainer>
+            </div>
+            <S.RightContainer>
+                <Tooltip direction="rightTop" content={<Prompt />}>
+                    <S.Content className="content">RT</S.Content>
+                </Tooltip>
+                <Tooltip direction="right" content={<Prompt />}>
+                    <S.Content className="rightBottom">Right</S.Content>
+                </Tooltip>
+                <Tooltip direction="rightBottom" content={<Prompt />}>
+                    <S.Content className="content">RB</S.Content>
+                </Tooltip>
+            </S.RightContainer>
+        </S.Container>
+    );
+}
